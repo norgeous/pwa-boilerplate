82bc025fa5452be74b1cb569468653b180aff950
Daniel Harte <norgeous@users.noreply.github.com>
2023-12-02 23:17:08 +0000

    Update vite.config.js

--- packages/vite-config/vite.config.js
+++ packages/vite-config/vite.config.js
@@ -4,5 +4,6 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // make index.html urls relative for deployment to GH pages
   plugins: [basicSsl(), react()],
 });
