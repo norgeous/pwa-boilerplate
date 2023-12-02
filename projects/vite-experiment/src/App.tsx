0bfeba73f979818f196e6590c1da500702fb5ff2
Daniel Harte <norgeous@users.noreply.github.com>
2023-12-02 23:50:49 +0000

    remove logos

--- projects/vite-experiment/src/App.tsx
+++ projects/vite-experiment/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 
 import Button from '@pwa/react-components/Button';
 import util from '@pwa/react-components/util';
@@ -14,14 +12,6 @@ const App = () => {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <Button>PWABUTTON</Button>
       <div className="card">
