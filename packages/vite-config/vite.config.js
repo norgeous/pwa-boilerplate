84209bb99834768d8656c0cbc63629723e762a4a
Daniel Harte <norgeous@users.noreply.github.com>
2023-12-02 22:17:44 +0000

    Update vite.config.js

--- packages/vite-config/vite.config.js
+++ packages/vite-config/vite.config.js
@@ -5,14 +5,4 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [basicSsl(), react()],
-
-  // @pwa/react-components exports jsx content with .js file extention
-  // there is a performance penality for doing this
-  // optimizeDeps: {
-  //   esbuildOptions: {
-  //     loader: {
-  //       '.js': 'jsx',
-  //     },
-  //   },
-  // },
 });
