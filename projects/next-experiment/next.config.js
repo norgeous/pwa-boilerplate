cc913bc66bc48c5f7ffe657314621490461868d6
Daniel Harte <norgeous@users.noreply.github.com>
2023-12-03 16:22:57 +0000

    add next-experiment

---
+++ projects/next-experiment/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // build nextjs project to static files
+  distDir: 'dist', // by default nextjs outputs to "out/"
+};
+
+module.exports = nextConfig;
