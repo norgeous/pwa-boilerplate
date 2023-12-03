a443c6d9c546a9628f2b2e94508a2bb27c920f7e
Daniel Harte <norgeous@users.noreply.github.com>
2023-12-03 17:06:13 +0000

    tidy

--- projects/next-experiment/src/app/page.tsx
+++ projects/next-experiment/src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import styles from './page.module.css';
 import CountButton from './CountButton';
 import Button from '@pwa/react-components/Button';
@@ -9,17 +8,6 @@ const Home = () => {
       <div className={styles.description}>
         <CountButton />
         <Button>test library button</Button>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
       </div>
     </main>
   );
