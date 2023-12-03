117212be6596eb3bc269659cdea8080ab241408f
Daniel Harte <norgeous@users.noreply.github.com>
2023-12-03 17:45:39 +0000

    relative urls in nextjs static build

--- projects/next-experiment/next.config.js
+++ projects/next-experiment/next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   output: 'export', // build nextjs project to static files
   distDir: 'dist', // by default nextjs outputs to "out/"
+  basePath: '/BASEPATH/', // postbuild adjusts this to relative url
 };
 
 module.exports = nextConfig;
