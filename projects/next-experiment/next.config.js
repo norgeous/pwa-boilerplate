8c5ce447a1478e1b3a7562fc4653a1d6ddbe7bd3
Daniel Harte <norgeous@users.noreply.github.com>
2023-12-05 19:48:19 +0000

    Update next.config.js

--- projects/next-experiment/next.config.js
+++ projects/next-experiment/next.config.js
@@ -2,6 +2,7 @@ const isDev = process.env.NODE_ENV === 'development';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  trailingSlash: true, // enforce trailing slash on urls
   output: 'export', // build nextjs project to static files
   distDir: 'dist', // by default nextjs outputs to "out/"
   ...(!isDev && {
